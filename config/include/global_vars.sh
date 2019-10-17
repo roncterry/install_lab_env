2369324193c3ee069ab213f290f9e136452f5dbe
roncterry <15840212+roncterry@users.noreply.github.com>
2019-10-16 23:38:29 -0600

    made global vars more consistent

--- config/include/global_vars.sh
+++ config/include/global_vars.sh
@@ -1,13 +1,16 @@
 ##############################################################################
 #                          Global Variables
-# version: 2.0.0
-# date: 2018-08-24
+# version: 3.0.0
+# date: 2019-10-15
 ##############################################################################
 
 CONFIG_SRC_DIR="./config"
 
 PDF_SRC_DIR="./pdf"
 PDF_DEST_DIR="${HOME}/pdf"
+
+COURSE_FILES_SRC_DIR="./course_files"
+COURSE_FILES_DEST_DIR="${HOME}/course_files"
 
 SCRIPTS_SRC_DIR="./scripts"
 SCRIPTS_DEST_DIR="${HOME}/scripts"
@@ -19,7 +22,7 @@ LAB_SCRIPT_DIR="lab-automation"
 DEPLOY_CLOUD_SCRIPT_DIR="deploy-cloud"
 
 VM_SRC_DIR="./VMs"
-VM_DEST_DIR="/home/VMs/${COURSE_NUM}"
+VM_DEST_DIR="/home/VMs"
 
 ISO_SRC_DIR="./iso"
 ISO_DEST_DIR="/home/iso"
